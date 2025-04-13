2c0a1a086ce943a11516584588ba576a41b0a4fd
evoexo <dragon520111back@gmail.com>
2025-04-13 10:29:25 -0400

    Building UML for hw 2.

--- Homework/Homework 2/src/Question.java
+++ Homework/Homework 2/src/Question.java
@@ -1,5 +1,11 @@
-public abstract class Question {
+import java.io.Serializable;
+
+public abstract class Question implements Serializable {
     protected String prompt;
     abstract public void displayQuestion();
     abstract public void takeQuestion();
+
+    public void setPrompt(String prompt) {
+
+    }
 }
---
+++ Homework/Homework 2/src/ValidDate.java
@@ -0,0 +1,2 @@
+public class ValidDate extends Question{
+}

ad24987bf0ba05da7eb21d71064b61d832606d95
Dawson De Mond <dragon520111@gmail.com>
2025-04-17 11:02:41 -0400

    Framework for object serialization

---
+++ Homework/Homework 2/src/PersistenceHandler.java
@@ -0,0 +1,11 @@
+public class PersistenceHandler {
+    private final String SER_DES_EXTENSION = ".ser";
+
+    public void serialize(Object object, String fileName) throws Exception {
+        throw new Exception("Not implemented");
+    }
+
+    public Object deserialize(String fileName) throws Exception {
+        throw new Exception("Not implemented");
+    }
+}

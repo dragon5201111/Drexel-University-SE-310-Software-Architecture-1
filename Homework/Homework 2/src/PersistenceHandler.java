d64c93a76e9ba05552bf01a98c1f927cab94a4ca
Dawson De Mond <dragon520111@gmail.com>
2025-04-17 12:55:59 -0400

    Implemented PersistenceHandler.java and restructured project.

--- Homework/Homework 2/src/PersistenceHandler.java
+++ Homework/Homework 2/src/PersistenceHandler.java
@@ -1,11 +1,32 @@
+import java.io.*;
+
 public class PersistenceHandler {
-    private final String SER_DES_EXTENSION = ".ser";
+    private String SER_DES_EXTENSION = ".ser";
 
     public void serialize(Object object, String fileName) throws Exception {
-        throw new Exception("Not implemented");
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(fileName + SER_DES_EXTENSION);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(object);
+            objectOutputStream.close();
+            fileOutputStream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public Object deserialize(String fileName) throws Exception {
-        throw new Exception("Not implemented");
+        fileName = fileName + SER_DES_EXTENSION;
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(fileName);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            Object returnObject = objectInputStream.readObject();
+            objectInputStream.close();
+            fileInputStream.close();
+            return returnObject;
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

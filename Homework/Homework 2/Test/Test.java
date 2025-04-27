ce477645d20902f0658bda97b13eea1df0812f73
evoexo <dragon520111back@gmail.com>
2025-04-27 11:22:36 -0400

    Added review images

--- Homework/Homework 2/Test/Test.java
+++ Homework/Homework 2/Test/Test.java
@@ -4,33 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Test {
-    public static Matching constructMatchingQuestion(){
-        List<String> leftSet = new ArrayList<>();
-        List<String> rightSet = new ArrayList<>();
-        leftSet.add("Minecraft");
-        rightSet.add("Video Games");
-        leftSet.add("Java");
-        rightSet.add("C++");
-        leftSet.add("Colors");
-        rightSet.add("Vision");
-        leftSet.add("Sound");
-        rightSet.add("Whistling");
-        return new Matching("Match Like Objects", leftSet, rightSet);
-    }
-
-    public static MultipleChoice constructMultipleChoiceQuestion(){
-        List<String> choices = new ArrayList<>();
-        choices.add("Star Wars");
-        choices.add("Video Games");
-        choices.add("C");
-        choices.add("Vision");
-        choices.add("Cars");
-        choices.add("Life is cool");
-        return new MultipleChoice("Select 4", choices, 4);
-    }
 
     public static void main(String[] args) {
-        SurveyMenu surveyMenu = new SurveyMenu();
-        surveyMenu.start();
+
     }
 }
